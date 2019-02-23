35b051d60b3ceeeda30ce9da02b9608c6b2067bd
bobiOne <bobione@abv.bg>
2019-02-23 14:35:07 +0200

    Update CommandProcessor.js

--- AdvanceedFunctions_Lab/CommandProcessor.js
+++ AdvanceedFunctions_Lab/CommandProcessor.js
@@ -1,33 +1,26 @@
-// function commandProcessor(arr) {
-//     let commandProcessor = (() => {
-//         let output = '';
+ function commandProcessor(arr) {
+     let commandProcessor = (() => {
+         let output = '';
 
-//         return {
-//             append: (str) => output = output + str,
-//             removeStart: (num) => output = output.slice(num),
-//             removeEnd: (num) => output = output.substring(0, output.length - num),
-//             print: () => console.log(output)
-//         };
-//     })();    
+         return {
+             append: (str) => output = output + str,
+             removeStart: (num) => output = output.slice(num),
+             removeEnd: (num) => output = output.substring(0, output.length - num),
+             print: () => console.log(output)
+         };
+     })();    
 
-//     for (let str of arr) {
-//         let [command, value] = str.split(' ');
-//         commandProcessor[command](value);
-//     }
-// }
+     for (let str of arr) {
+         let [command, value] = str.split(' ');
+         commandProcessor[command](value);
+     }
+ }
 
-// commandProcessor([
-//     "append hello",
-//     "append again",
-//     "removeStart 3",
-//     "removeEnd 4",
-//     "print"
-// ]);
-
-let f = (function() {
-    let counter = 0;
-    return function() {
-      console.log(++counter);
-    };
-  })();
+ commandProcessor([
+     "append hello",
+     "append again",
+     "removeStart 3",
+     "removeEnd 4",
+     "print"
+ ]);
   
